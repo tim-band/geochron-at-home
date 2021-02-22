c741afad904dd05a1247935a29945b996e05f49e
Tim Band <t.band>
2021-02-22 20:00:12 +0000

    Many tests

--- test/integration.py
+++ test/integration.py
@@ -1,43 +1,174 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import subprocess
 
-# We are assuming that the following has already been done:
-# docker-compose -f docker-compose-test.yml down
-# docker-compose -f docker-compose-test.yml up --force-recreate --build -d
-# docker-compose -f docker-compose-test.yml exec django ./site_init.sh
+class DockerCompose:
+    def __init__(self, yml):
+        if yml:
+            self.pre = ["docker-compose", "-f", yml]
+        else:
+            self.pre = ["docker-compose"]
 
-driver = webdriver.Firefox()
-driver.get("http://localhost:18080/ftc")
+    def down(self):
+        subprocess.run(self.pre + ["down"])
+        return self
 
-# Click Join button (maybe we should log out first?)
-join_button = driver.find_element_by_css_selector("a.btn-success")
-join_button.click()
+    def up(self):
+        subprocess.run(self.pre + ["up", "--force-recreate", "--build", "-d"])
+        return self
 
-# Fill in details
-test_email_address = "tester@test.com"
-test_user_id = "tester"
-driver.find_element_by_id("id_username").send_keys(test_user_id)
-driver.find_element_by_id("id_email").send_keys(test_email_address)
-driver.find_element_by_id("id_password1").send_keys("MyPaSsW0rd")
-driver.find_element_by_id("id_password2").send_keys("MyPaSsW0rd")
-driver.find_element_by_class_name("btn-primary").click()
+    def exec(self, *kargs):
+        subprocess.run(self.pre + ["exec", "django"] + list(kargs))
+        return self
 
-# Check that we are on the verify page now
-assert "Verify" in driver.title
-info = driver.find_element_by_css_selector("div.alert-info")
-assert test_email_address in info.text
+    def init(self):
+        self.exec("./site_init.sh")
+        return self
 
-# Check webmail
-driver.get("http://localhost:18081/")
 
-# Click on the first link in the first email we see, that's what I always do
-driver.find_element_by_css_selector("td.body-text p a").click()
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
 
-# Does this display the correct message?
-assert test_user_id in driver.find_element_by_css_selector("p.lead span.lead").text
-assert test_email_address in driver.find_element_by_css_selector("p.lead > a").text
 
-# This is correct! Click "confirm"
-driver.find_element_by_css_selector("button.btn-success").click()
+class WebMail(BasePage):
+    def go(self):
+        self.driver.get("http://localhost:18081/")
+        return self
 
-driver.close()
+    def click_first_body_link(self):
+        self.driver.find_element_by_css_selector("td.body-text p a").click()
+
+
+class HomePage(BasePage):
+    def go(self):
+        self.driver.get("http://localhost:18080/ftc")
+        return self
+
+    def join(self):
+        self.driver.find_element_by_css_selector("a.btn-success").click()
+        return JoinPage(self.driver)
+
+
+class User:
+    def __init__(self, identity, email, password):
+        self.identity = identity
+        self.email = email
+        self.password = password
+
+
+class JoinPage(BasePage):
+    def check(self):
+        assert "Signup" in self.driver.title
+        return self
+
+    def fill_in(self, user):
+        self.driver.find_element_by_id("id_username").send_keys(user.identity)
+        self.driver.find_element_by_id("id_email").send_keys(user.email)
+        self.driver.find_element_by_id("id_password1").send_keys(user.password)
+        self.driver.find_element_by_id("id_password2").send_keys(user.password)
+        self.driver.find_element_by_class_name("btn-primary").click()
+        return VerifyPage(self.driver)
+
+
+class VerifyPage(BasePage):
+    def check(self, user):
+        assert "Verify" in self.driver.title
+        info = self.driver.find_element_by_css_selector("div.alert-info")
+        assert user.email in info.text
+        return self
+
+
+class ConfirmPage(BasePage):
+    def check(self, user):
+        assert user.identity in self.driver.find_element_by_css_selector("p.lead span.lead").text
+        assert user.email in self.driver.find_element_by_css_selector("p.lead > a").text
+        return self
+
+    def confirm(self):
+        self.driver.find_element_by_css_selector("button.btn-success").click()
+        return SignInPage(self.driver)
+
+
+class SignInPage(BasePage):
+    def go(self):
+        self.driver.get("http://localhost:18080/accounts/login")
+        return self
+
+    def sign_in(self, user):
+        self.driver.find_element_by_css_selector('input[name="login"]').send_keys(user.identity)
+        self.driver.find_element_by_css_selector('input[name="password"]').send_keys(user.password)
+        self.driver.find_element_by_class_name("btn-primary").click()
+        return ProfilePage(self.driver)
+
+
+class ProfilePage(BasePage):
+    def login_name(self):
+        return self.driver.find_element_by_class_name('fa-user').text
+
+    def go_start_counting(self):
+        self.driver.find_element_by_id("start-counting-link").click()
+        return CountingPage(self.driver)
+
+
+class CountingPage(BasePage):
+    def check(self):
+        self.driver.find_element_by_id("btn-tracknum")
+        return self
+
+    def count(self):
+        return self.driver.execute_script(
+            'return document.getElementById("tracknum").value;')
+
+    def click_at(self, x, y):
+        mp = self.driver.find_element_by_id("map")
+        actions = ActionChains(self.driver)
+        actions.move_to_element_with_offset(mp, x, y)
+        actions.click().pause(1.1).perform()
+        return self
+
+
+class DjangoTests(unittest.TestCase):
+    def setUp(self):
+        self.dc = DockerCompose("./docker-compose-test.yml").down().up().init()
+        self.driver = webdriver.Firefox()
+
+    def test_onboard(self):
+        # upload x-rays
+        self.dc.exec("python3", "upload_projects.py",
+            "-s", "geochron.settings",
+            "-i", "test/xrays",
+            "-o", "static/grain_pool")
+
+        # create user
+        join_page = HomePage(self.driver).go().join()
+        self.test_user = User("tester", "tester@test.com", "MyPaSsW0rd")
+        join_page.check().fill_in(self.test_user).check(self.test_user)
+        WebMail(self.driver).go().click_first_body_link()
+        ConfirmPage(self.driver).check(self.test_user).confirm()
+
+        # sign in as this new user
+        profile = SignInPage(self.driver).go().sign_in(self.test_user)
+
+        # start counting tracks
+        counting = profile.go_start_counting().check()
+        self.assertEqual(counting.count(), "000")
+        counting.click_at(120, 70)
+        self.assertEqual(counting.count(), "001")
+        counting.click_at(90, 100)
+        self.assertEqual(counting.count(), "002")
+        # this one is outside of the boundary
+        counting.click_at(110, 110)
+        self.assertEqual(counting.count(), "002")
+        counting.click_at(105, 75)
+        self.assertEqual(counting.count(), "003")
+
+    def tearDown(self):
+        self.driver.close()
+        self.dc.down()
+
+
+if __name__ == '__main__':
+    unittest.main()
