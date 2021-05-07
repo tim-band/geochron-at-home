85bdc557fb9abbb833c3a6e08632feb0c23a3904
Tim Band <t.band>
2021-05-07 16:41:00 +0100

    proj and site admin start preverified

--- site_default_users.py
+++ site_default_users.py
@@ -7,6 +7,7 @@ django.setup()
 
 from django.contrib.sites.models import Site
 from django.contrib.auth.models import User
+from allauth.account.models import EmailAddress
 
 site = Site.objects.get(id=1)
 site.domain = os.getenv('SITE_DOMAIN')
@@ -17,7 +18,9 @@ uname = os.getenv("SITE_ADMIN_NAME")
 if not User.objects.filter(username=uname).exists():
     email_addr = os.getenv("SITE_ADMIN_EMAIL")
     passwd = os.getenv("SITE_ADMIN_PASSWORD")
-    User.objects.create_superuser(uname, email_addr, passwd)
+    user = User.objects.create_superuser(uname, email_addr, passwd)
+    av = EmailAddress(user=user, email=email_addr, verified=True)
+    av.save()
     print("Super user `{name}` created.".format(name=uname))
 
 uname = os.getenv("PROJ_ADMIN_NAME")
@@ -27,6 +30,8 @@ if not User.objects.filter(username=uname).exists():
     projAdmin = User.objects.create_user(uname, email=email_addr, password=passwd)
     projAdmin.is_staff=True
     projAdmin.save()
+    av = EmailAddress(user=projAdmin, email=email_addr, verified=True)
+    av.save()
     print("projects upload user `{name}` created.".format(name=uname))
 
 if not User.objects.filter(username='guest').exists():
