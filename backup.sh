61cb06ecfbb5705b921bd3b4725f89daee6584db
Tim Band <t.band>
2021-06-15 12:55:49 +0100

    backup script

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Backup the geochron database to /var/local/geochron/backup<DATE>.gz
+# To restore, use:
+# gunzip -c /var/local/geochron/backup20211202.gz | docker-compose exec -T db psql -U geochron -f-
+mkdir -p /var/local/geochron
+cd `dirname $0`
+docker-compose exec db pg_dump -c -U geochron | gzip -c > /var/local/geochron/backup`date +%Y%m%d`.gz
+# delete backups more than a week old
+find /var/local/geochron/ -mtime +7 -delete
