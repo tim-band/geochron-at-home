405f7ab68b73d053acf9acb569e14b2639ee99f7
tim-band <t@u>
2021-06-29 13:28:28 +0000

    Backups work from cron job

--- backup.sh
+++ backup.sh
@@ -4,6 +4,6 @@
 # gunzip -c /var/local/geochron/backup20211202.gz | docker-compose exec -T db psql -U geochron -f-
 mkdir -p /var/local/geochron
 cd `dirname $0`
-docker-compose exec db pg_dump -c -U geochron | gzip -c > /var/local/geochron/backup`date +%Y%m%d`.gz
+docker-compose exec -T db pg_dump -c -U geochron | gzip -c > /var/local/geochron/backup`date +%Y%m%d`.gz
 # delete backups more than a week old
 find /var/local/geochron/ -mtime +7 -delete
