49d1e10bfe811dfaac2cae3c52d6dc3b10798d07
Tim Band <t.band@nnnn.com>
2022-09-15 18:51:09 +0100

    Counting page fixed, tests pass

--- ftc/test_selenium.py
+++ ftc/test_selenium.py
@@ -78,6 +78,14 @@ class BasePage:
             elt = self.driver.find_element(By.ID, elt)
         return 'leaflet-disabled' in elt.get_attribute('class').split(' ')
 
+    def scroll_into_view(self, elt):
+        r = elt.rect
+        wr = self.driver.get_window_rect()
+        js = 'window.scroll({0},{1})'.format(
+            r['x'] + (r['width'] - wr['width'])/2,
+            r['y'] + (r['height'] - wr['height'])/2
+        )
+        self.driver.execute_script(js)
 
 class WebMail(BasePage):
     def go(self):
@@ -163,7 +171,9 @@ class TutorialPage(BasePage):
         return self
 
     def go_next(self):
-        self.driver.find_element(By.ID, 'next').click()
+        e = self.driver.find_element(By.ID, 'next')
+        self.scroll_into_view(e)
+        e.click()
         return self
 
     def go_previous(self):
@@ -324,8 +334,10 @@ class CountingPage(BasePage):
                     return False
                 return es
 
+        # we used to wait for all four to be loaded, but now it seems only one
+        # gets loaded at a time.
         images = WebDriverWait(self.driver, 3).until(
-            ElementsLoaded(By.CLASS_NAME, "leaflet-image-layer", 4))
+            ElementsLoaded(By.CLASS_NAME, "leaflet-image-layer", 1))
 
         return images[-1].get_attribute("src")
 
