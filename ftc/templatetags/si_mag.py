f3b1f9a578ef39c23b593f8030440d38693cc11f
Tim Band <t.band@nnnn.com>
2022-09-29 13:05:30 +0100

    Pixel Size gets SI units

---
+++ ftc/templatetags/si_mag.py
@@ -0,0 +1,27 @@
+from django.template import Library
+
+register = Library()
+
+large = ['k', 'M', 'G']
+small = ['m', '\xb5', 'n', 'p']
+
+@register.simple_tag
+def si_mag(num, decimal_places=2):
+    sign = ''
+    if num < 0:
+        num = -num
+        sign = '-'
+    mag = 0
+    while mag < len(large) and 1000 <= num:
+        num /= 1000
+        mag += 1
+    while -mag < len(small) and num < 1:
+        num *= 1000
+        mag -= 1
+    prefix = ''
+    if mag < 0:
+        prefix = small[-1 - mag]
+    elif 0 < mag:
+        prefix = large[mag - 1]
+    format_string = '{0}{1:.' + str(decimal_places) + 'f}{2}'
+    return format_string.format(sign, num, prefix)
